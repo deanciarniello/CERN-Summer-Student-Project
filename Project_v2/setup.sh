7c60230f8ac423530712214c53642858e174131a
deanciarniello <deancciarniello@gmail.com>
2023-08-10 15:58:13 +0200

    Added setup file to Projectv2

---
+++ Project_v2/setup.sh
@@ -0,0 +1,33 @@
+# File: setup.sh
+# Author: Dean Ciarniello
+# Date: 2023-08-09
+
+#!/bin/bash
+
+# Setup Script (for lxplus)
+#=====================================================
+echo "beginning setup.sh"
+
+# Source geant4 and gcc
+#=====================================================
+echo "sourcing geant4 and gcc (compiler)"
+source /cvmfs/sft.cern.ch/lcg/contrib/gcc/10.3.0/x86_64-centos7/setup.sh
+source /cvmfs/geant4.cern.ch/geant4/10.7.ref09/x86_64-centos7-gcc10-optdeb/CMake-setup.sh
+
+# Setup fresh build directory
+#=====================================================
+echo "making fresh build directory"
+if [ -d "build/" ]; then
+    rm -r build/
+fi
+mkdir build
+cd build
+
+# Compile Simulation
+#=====================================================
+echo "compiling simulation"
+cmake3 ..
+make
+cd ..
+
+echo "done setup.sh"
